b3d60930a3e9e0023967a1b34e46b26bf8efc6d8
kyky <kylian.rigaud123@gmail.com>
2023-11-15 12:51:38 +0100

    css pas réglé

--- serveur.js
+++ serveur.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io =  require("socket.io")(server); // bun
-// const io = new require("socket.io")(serveur); // node.js
+//const io =  require("socket.io")(server); // bun
+const io = new require("socket.io")(server); // node.js
 
 // declare toutes tes variables avec 'let' stp 
 // 'var' c'est pareil mais en moins bien
 
 
 // app get
+
 server.listen(8888, () => {
     console.log('Le serveur écoute sur le port 8888');
 });
@@ -58,7 +59,6 @@ io.on("connection",(socket)=>{
 
     // hex
     socket.on("auchargement",()=>{
-        socket.emit("auchargement",carreclick);
     });
     
     socket.on("oncarre",data=>{
