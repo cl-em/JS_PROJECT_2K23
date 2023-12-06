7f0a047a03138f9c3742d6ea9226f6631ffd8dc1
kyky <kylian.rigaud123@gmail.com>
2023-12-06 19:32:10 +0100

    html, css + déplacement vers taniere

--- serveur.js
+++ serveur.js
@@ -13,7 +13,7 @@ class Animal{
     constructor(p){
         this.sexe=false;
         this.position=p;
-        this.stats={eau:4,faim:4};
+        this.stats={eau:5,faim:5};
     }
 
     enVie(){ /*Méthode*/
@@ -704,6 +704,225 @@ const bordureG = Array.from({ length: 13 }, (_, index) => 13 * index); /*Liste c
                 }
         }
 
+    function deplacementTanierePlusProche(joueur, animal){
+            let perception  = joueur.precep //stats a ajouter dans la classe animale pour pouvoir witch dessus
+            let listeDirection = [deplacementPrairiePlusProche, deplacementEauPlusProche] //liste qui contient toutes les fonctions de déplacements
+            let listeTaniereP1 = [];
+            let d = false;
+        
+            if(cases[animal.position]=="taniere"){ //cas ou l'animal est deja sur de l'taniere
+                resteSurPlace(animal);
+            }
+            else{
+                    //Cas ou l'animal a 1 de perception, si il voit seulement les cases autour de lui
+        
+                    if((cases[animal.position-13]=="taniere") && perception>0) //Ajoute ssi en haut a gauche c'est de l'taniere
+                        listeTaniereP1.push(animal.position-13);
+        
+                    if((cases[animal.position-12]=="taniere") && perception>0) //Ajoute ssi en haut a droite c'est de l'taniere
+                        listeTaniereP1.push(animal.position-12);
+        
+                    if((cases[animal.position-1]=="taniere") && perception>0) //Ajoute ssi a gauche c'est de l'taniere
+                        listeTaniereP1.push(animal.position-1);
+        
+                    if((cases[animal.position+1]=="taniere") && perception>0) //Ajoute ssi a droite c'est de l'taniere
+                        listeTaniereP1.push(animal.position+1);
+        
+                    if((cases[animal.position+12]=="taniere") && perception>0) //Ajoute ssi en bas a gauche c'est de l'taniere
+                        listeTaniereP1.push(animal.position+12);
+        
+                    if((cases[animal.position+13]=="taniere") && perception >0) //Ajoute ssi en bas a droite c'est de l'taniere
+                        listeTaniereP1.push(animal.position+13);
+        
+                    if(listeTaniereP1.length > 0){ //Va aléatoirement sur une case taniere, si il en existe une
+                        let rndmP1 = listeTaniereP1[Math.floor(Math.random() * listeTaniereP1.length)];
+                        animal.position = rndmP1;
+                        animal.stats.taniere -= 1;
+                        animal.stats.faim -= 0.50;
+                        d = true;
+                    }
+        
+                    //Cas ou l'animal a 2 de perceptions
+        
+                    if(cases[animal.position-26]=="taniere" && perception > 1 && d == false){ //se déplace en haut a gauche si taniere deux fois en haut a gauche
+                        deplacementHautGauche(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-24]=="taniere" && perception > 1 && d == false){ //se déplace en haut a droite si taniere deux fois en haut a droite
+                        deplacementHautDroite(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-2]=="taniere" && perception > 1 && d == false){ //se déplace a gauche si taniere deux fois a gauche
+                        deplacementGauche(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+2]=="taniere" && perception > 1 && d == false){ //se déplace a droite si taniere deux fois a droite
+                        deplacementDroite(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+24]=="taniere" && perception > 1 && d == false){ //se déplace en bas a gauche si taniere deux fois en bas a gauche
+                        deplacementBasGauche(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+26]=="taniere" && perception > 1 && d == false){ //se déplace en bas a droite si taniere deux fois en bas a droite
+                        deplacementBasDroite(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-14]=="taniere" && perception > 1 && d == false){ //se déplace en haut a gauche ou a gauche si taniere en haut a gauche + gauche
+                        let choix1 = choisirDeplacementEntreDeux(deplacementGauche, deplacementHautGauche);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-11]=="taniere" && perception > 1 && d == false){ //se déplace en haut a droite ou a droite si taniere en haut a droite + droite
+                        let choix1 = choisirDeplacementEntreDeux(deplacementDroite, deplacementHautDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-11]=="taniere" && perception > 1 && d == false){ //se déplace en bas a gauche ou a gauche si taniere en bas a gauche + gauche
+                        let choix1 = choisirDeplacementEntreDeux(deplacementGauche, deplacementBasGauche);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+14]=="taniere" && perception > 1 && d == false){ //se déplace en bas a droite ou a droite si taniere en bas a droite + droite
+                        let choix1 = choisirDeplacementEntreDeux(deplacementDroite, deplacementBasDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-25]=="taniere" && perception > 1 && d == false){ //se déplace en haut a gauche ou en haut a droite si taniere en haut a gauche + en haut a droite ou en haut a droite + en haut a gauche
+                        let choix1 = choisirDeplacementEntreDeux(deplacementHautGauche, deplacementHautDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+25]=="taniere" && perception > 1 && d == false){ //se déplace en bas a gauche ou en bas a droite si taniere en bas a gauche + en bas a droite ou en bas a droite + en bas a gauche
+                        let choix1 = choisirDeplacementEntreDeux(deplacementBasGauche, deplacementBasDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    //Cas ou l'animal a 3 de perception
+                    if(cases[animal.position-39]=="taniere" && perception > 2 && d == false){ //1
+                        deplacementHautGauche(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-36]=="taniere" && perception > 2 && d == false){ //2 
+                        deplacementHautDroite(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+36]=="taniere" && perception > 2 && d == false){ //3
+                        deplacementBasGauche(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+39]=="taniere" && perception > 2 && d == false){ //4
+                        deplacementBasDroite(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+3]=="taniere" && perception > 2 && d == false){ //5
+                        deplacementDroite(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-3]=="taniere" && perception > 2 && d == false){ //6
+                        deplacementGauche(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+27]=="taniere" && perception > 2 && d == false){  //7
+                        let choix1 = choisirDeplacementEntreDeux(deplacementDroite, deplacementBasDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+15]=="taniere" && perception > 2 && d == false){  //8
+                        let choix1 = choisirDeplacementEntreDeux(deplacementDroite, deplacementBasDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+10]=="taniere" && perception > 2 && d == false){ //9
+                        let choix1 = choisirDeplacementEntreDeux(deplacementGauche, deplacementBasGauche);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+23]=="taniere" && perception > 2 && d == false){ //10
+                        let choix1 = choisirDeplacementEntreDeux(deplacementGauche, deplacementBasGauche);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+37]=="taniere" && perception > 2 && d == false){ //11
+                        let choix1 = choisirDeplacementEntreDeux(deplacementBasGauche, deplacementBasDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position+38]=="taniere" && perception > 2 && d == false){ //12
+                        let choix1 = choisirDeplacementEntreDeux(deplacementBasGauche, deplacementBasDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-10]=="taniere" && perception > 2 && d == false){ //13
+                        let choix1 = choisirDeplacementEntreDeux(deplacementDroite, deplacementHautDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-23]=="taniere" && perception > 2 && d == false){ //14
+                        let choix1 = choisirDeplacementEntreDeux(deplacementDroite, deplacementHautDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-15]=="taniere" && perception > 2 && d == false){ //15
+                        let choix1 = choisirDeplacementEntreDeux(deplacementGauche, deplacementHautGauche);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-27]=="taniere" && perception > 2 && d == false){ //16
+                        let choix1 = choisirDeplacementEntreDeux(deplacementGauche, deplacementHautGauche);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-38]=="taniere" && perception > 2 && d == false){ //17
+                        let choix1 = choisirDeplacementEntreDeux(deplacementHautGauche, deplacementHautDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+                    else if(cases[animal.position-37]=="taniere" && perception > 2 && d == false){ //18
+                        let choix1 = choisirDeplacementEntreDeux(deplacementHautGauche, deplacementHautDroite);
+                        choix1(animal);
+                        d = true;
+                    }
+        
+        
+        
+                    else if(d == false){ //Si je vois aucune case d'taniere, je me déplace aléatoirement
+                        let deplacementRndm = listeDirection[Math.floor(Math.random() * listeDirection.length)];
+                        deplacementRndm(animal);
+                        d = true;
+                    }
+                }
+        }
+
     //--------------------------------------------------------------------------------------------------------
 
     function jouerTour() { /*Fonction qui permet de gérer chaque tour, les déplacements..*/
@@ -711,18 +930,16 @@ const bordureG = Array.from({ length: 13 }, (_, index) => 13 * index); /*Liste c
             if (animaux[value.name]) {
                 animaux[value.name].forEach((animal) => {
 
-                    if(animal.stats.eau <= 6){
-                        deplacementEauPlusProche(value, animal);
+                    if(animal.stats.eau >= 6 && animal.stats.faim > 6){
+                        deplacementTanierePlusProche(value, animal);
                     }
 
-                    else if(animal.stats.faim <= 6){
-                        deplacementPrairiePlusProche(value, animal);
+                    else if(animal.stats.faim > 8){
+                        deplacementEauPlusProche(value, animal);
                     }
                     else{
-                        let listeDirection = [deplacementHautGauche, deplacementHautDroite, deplacementGauche, deplacementDroite, deplacementBasGauche, deplacementBasDroite];
-                        let deplacementRndm = listeDirection[Math.floor(Math.random() * listeDirection.length)];
-                        deplacementRndm(animal);
-                    }
+                        deplacementPrairiePlusProche(value, animal);
+                    }   
                     
                     if(cases[animal.position]=="prairie"){ /*Nourrit l'animal si il se trouve sur de la prairie et re-set ses stats a 10 si il dépasse (10 étant le seuil)*/
                         animal.stats.faim+=2;
